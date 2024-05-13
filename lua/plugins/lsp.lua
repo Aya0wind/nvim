e8a63219c4e31fbdb2f04418c683e8d7e47f81d6
abc66789 <120035959@qq.com>
2024-05-13 15:12:01 +0800

    enable inlay hints only nvim 0.10

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -130,7 +130,13 @@ return {
 	{
 		"MysticalDevil/inlay-hints.nvim",
 		event = "LspAttach",
-		enabled = false,
+		enabled = function()
+			if vim.fn.has("nvim-0.10") == 1 then
+				return true
+            else 
+                return false
+            end
+		end,
 		dependencies = { "neovim/nvim-lspconfig" },
 		config = function()
 			require("inlay-hints").setup()
